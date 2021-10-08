51bd765e44258b96864a8354086e521f25dd67b9
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2021-10-07 23:50:15 -0700

    Utility Belt and Andy Z
    - Scripts
    - models

--- Contents/mods/Authentic Z/media/lua/shared/NPCs/AuthenticZ_ZombieZoneDefinitions.lua
+++ Contents/mods/Authentic Z/media/lua/shared/NPCs/AuthenticZ_ZombieZoneDefinitions.lua
@@ -1153,6 +1153,12 @@ ZombiesZoneDefinition.MovieNight = {
 		mandatory="true",
 		gender="male",
 	},
+	AuthenticAuthenticAndyDotDM = {
+		name="AuthenticAndyDotD",
+		toSpawn=1,
+		mandatory="true",
+		gender="male",
+	},	
 }
 
 ZombiesZoneDefinition.Nate = {
